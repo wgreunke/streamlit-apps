70fb96dc7d092915cdb773d0c39a5e52b775e543
wgreunke <wgreunke@gmail.com>
2024-02-12 10:15:01 -0800

    Update test_streamlit.py

--- test_streamlit.py
+++ test_streamlit.py
@@ -9,7 +9,18 @@ df = pd.DataFrame({
   'second column': [11, 20, 30, 40]
 })
 
+""" *****************UI Elements********************* 
+"""
 age = st.slider('How old are you?', 0, 130, 25)
+st.scatter_chart(chart_data,x='col1',y='col2',color='col4',size='col3',)
+
+
+# *******************  Server **********************
 st.write("I'm ", age, 'years old')
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["col1", "col2", "col3"])
+chart_data['col4'] = np.random.choice(['A','B','C'], 20)
+
+
 
 df
